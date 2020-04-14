4ac2a7df1ce8dec69ed1abe34fa1eb984313d5a0
liyatang <liyatang@qq.com>
2020-04-14 14:45:02 +0800

    c

--- packages/react/src/component/page/index.js
+++ packages/react/src/component/page/index.js
@@ -1,48 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import Flex from '../flex/index'
+import Flex from '../flex'
 
-class Page extends React.Component {
-  render() {
-    const {
-      className,
-      pageClassName,
-      white,
-      header,
-      tabbar,
-      top,
-      bottom,
-      children,
-      ...rest
-    } = this.props
-
-    return (
-      <Flex
-        {...rest}
-        column
-        className={classNames(
-          'm-page',
-          {
-            'm-page-white': white,
-          },
-          className
-        )}
-      >
-        {header && (
-          <Flex column none className='m-page-header m-flex-none'>
-            {header}
-          </Flex>
-        )}
-        {top && <div className='m-flex-none'>{top}</div>}
-        <div className={classNames('m-page-content', pageClassName)}>
-          {children}
-        </div>
-        {bottom && <div className='m-flex-none'>{bottom}</div>}
-        {tabbar && <div className='m-page-tabbar m-flex-none'>{tabbar}</div>}
-      </Flex>
-    )
-  }
+const Page = ({
+  className,
+  pageClassName,
+  white,
+  header,
+  tabbar,
+  top,
+  bottom,
+  children,
+  ...rest
+}) => {
+  return (
+    <Flex
+      {...rest}
+      column
+      className={classNames(
+        'm-page',
+        {
+          'm-page-white': white,
+        },
+        className
+      )}
+    >
+      {header && (
+        <Flex column none className='m-page-header m-flex-none'>
+          {header}
+        </Flex>
+      )}
+      {top && <div className='m-flex-none'>{top}</div>}
+      <div className={classNames('m-page-content', pageClassName)}>
+        {children}
+      </div>
+      {bottom && <div className='m-flex-none'>{bottom}</div>}
+      {tabbar && <div className='m-page-tabbar m-flex-none'>{tabbar}</div>}
+    </Flex>
+  )
 }
 
 Page.propTypes = {
