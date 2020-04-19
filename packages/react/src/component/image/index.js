1afd54fb7bc4ac89409c2b0cd08e4648be216469
electhuang <electhuang@gmail.com>
2020-04-19 22:23:47 +0800

    image

---
+++ packages/react/src/component/image/index.js
@@ -0,0 +1,103 @@
+import React, { useState, useRef } from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+const error = require('./error.png')
+const placeholder = require('./placeholder.png')
+
+const Image = (props) => {
+  const reloadCount = useRef(0)
+  const {
+    width,
+    height,
+    round,
+    objectFix,
+    className,
+    placeholder,
+    error,
+    ...rest
+  } = props
+  const [src, setSrc] = useState(props.src || placeholder)
+
+  const handleError = () => {
+    if (reloadCount.current < 2) {
+      reloadCount.current++
+      setTimeout(() => {
+        setSrc(src + `?${Math.random()}`)
+      }, 200)
+    } else {
+      setSrc(error)
+    }
+  }
+
+  const handleLoad = () => {
+    reloadCount.current = 0
+  }
+
+  const classNames = classnames(`m-image-${objectFix}`, className, {
+    'm-image-round': round,
+  })
+
+  return (
+    <img
+      {...rest}
+      src={src}
+      width={width}
+      height={height}
+      onError={handleError}
+      onLoad={handleLoad}
+      className={classNames}
+    />
+  )
+}
+
+Image.propTypes = {
+  src: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  round: PropTypes.bool,
+  objectFix: PropTypes.oneOf([
+    'contain',
+    'cover',
+    'fill',
+    'none',
+    'scale-down',
+  ]),
+  placeholder: PropTypes.string,
+  error: PropTypes.string,
+  className: PropTypes.string,
+}
+
+const ImageWrapper = (props) => <Image key={props.src} {...props} />
+
+ImageWrapper.propTypes = {
+  /** 图片地址 */
+  src: PropTypes.string,
+  /** 图片高度 */
+  height: PropTypes.number,
+  /** 图片宽度 */
+  width: PropTypes.number,
+  /** 是否为圆形 */
+  round: PropTypes.bool,
+  /** 填充模式 */
+  objectFix: PropTypes.oneOf([
+    'contain',
+    'cover',
+    'fill',
+    'none',
+    'scale-down',
+  ]),
+  /** 默认占位图地址 */
+  placeholder: PropTypes.string,
+  /** 加载出错占位图 */
+  error: PropTypes.string,
+  className: PropTypes.string,
+  style: PropTypes.object,
+}
+
+ImageWrapper.defaultProps = {
+  objectFix: 'fill',
+  placeholder: placeholder,
+  error: error,
+}
+
+export default ImageWrapper
