bc286cb3a960a3686aae8ddf1a061575c576fa6c
xumeimei <xumeimei@guanmai.cn>
2020-04-27 16:52:43 +0800

    border input

--- packages/react/src/component/input/index.js
+++ packages/react/src/component/input/index.js
@@ -1,5 +1,6 @@
 import Input from './input'
 import InputPassword from './input_password'
+import BorderInput from './border_input'
 
-export { Input, InputPassword }
+export { Input, InputPassword, BorderInput }
 export default Input
--- packages/react/src/component/input/stories.js
+++ packages/react/src/component/input/stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Input from './input'
 import InputPassword from './input_password'
 import { observable } from 'mobx'
+import BorderInput from './border_input'
 
 const store = observable({
   value: '',
@@ -17,6 +18,27 @@ export const normal = () => {
       onChange={(e) => store.setValue(e.target.value)}
       placeholder='请输入'
     />
+  )
+}
+
+export const border = () => {
+  return (
+    <div className='m-margin-10'>
+      默认形式
+      <BorderInput
+        value={store.value}
+        onChange={(e) => store.setValue(e.target.value)}
+        placeholder='请输入'
+      />
+      <br />
+      显示字数
+      <BorderInput
+        value={store.value}
+        maxLength={30}
+        onChange={(e) => store.setValue(e.target.value)}
+        placeholder='请输入'
+      />
+    </div>
   )
 }
 
