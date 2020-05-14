2c726a3393c7068da63bd6f23bfda60246612e41
liyatang <liyatang@qq.com>
2020-05-14 21:05:31 +0800

    TagWrap

--- packages/react/src/component/tag/index.js
+++ packages/react/src/component/tag/index.js
@@ -2,9 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-const TagWrap = ({ tag, className, children, ...rest }) => {
+const TagWrap = ({ tag, bottom, block, className, children, ...rest }) => {
   return (
-    <div {...rest} className={classNames('m-tag-wrap', className)}>
+    <div
+      {...rest}
+      className={classNames(
+        'm-tag-wrap',
+        {
+          'm-tag-wrap-bottom': bottom,
+          'm-tag-wrap-block': block,
+        },
+        className
+      )}
+    >
       {children}
       {tag && <div className='m-tag'>{tag}</div>}
     </div>
@@ -13,6 +23,8 @@ const TagWrap = ({ tag, className, children, ...rest }) => {
 
 TagWrap.propTypes = {
   tag: PropTypes.string.isRequired,
+  bottom: PropTypes.bool,
+  block: PropTypes.bool,
   className: PropTypes.string,
   style: PropTypes.object,
 }
