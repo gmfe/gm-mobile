ced55570af8553c88c17ea54d81ca39051237a05
electhuang <electhuang@gmail.com>
2020-06-12 15:42:10 +0800

    TabDateSelect

--- packages/business/src/component/tab_date_select/index.js
+++ packages/business/src/component/tab_date_select/index.js
@@ -1,17 +1,46 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { Tabs, Popup, RangeCalendar, Flex, Button } from '@gm-mobile/react'
+import {
+  Tabs,
+  Popup,
+  RangeCalendar,
+  Flex,
+  Button,
+  SelectPicker,
+} from '@gm-mobile/react'
 import { getLocale } from '@gm-mobile/locales'
 import _ from 'lodash'
 import moment from 'moment'
 import PropTypes from 'prop-types'
+import SvgArrowDown from '../../../svg/arrow-down.svg'
 
-const TabDateSelect = ({ tabs, selectedTab, begin, end, onSelect }) => {
+const getServiceTimeRange = (serviceTime) => {
+  const maxSpanEnd =
+    serviceTime.type === 2
+      ? serviceTime.receive_time_limit.e_span_time
+      : serviceTime.order_time_limit.e_span_time
+  const days30 = moment().add(30, 'd')
+  const daysWithSpan = moment().add(maxSpanEnd, 'd')
+  const maxTemp = daysWithSpan.isAfter(days30) ? days30 : daysWithSpan
+  return {
+    min: moment(maxTemp).add(-3, 'month'),
+    max: maxTemp,
+  }
+}
+
+const TabDateSelect = ({
+  tabs,
+  selectedTab,
+  begin,
+  end,
+  onSelect,
+  serviceTimeList,
+}) => {
   const calendarRef = useRef(null)
-  const [tabValue, setTabValue] = useState(selectedTab || tabs[0].value)
   const [beginDate, setBeginDate] = useState(begin)
   const [endDate, setEndDate] = useState(end)
-
-  const _selectedTab = _.find(tabs, (tab) => tab.value === tabValue)
+  const [activeTab, setActiveTab] = useState(
+    _.find(tabs, (tab) => tab.value === selectedTab) || tabs[0]
+  )
 
   useEffect(() => {
     setTimeout(() => {
@@ -21,26 +50,77 @@ const TabDateSelect = ({ tabs, selectedTab, begin, end, onSelect }) => {
 
   const handleSaveSelect = () => {
     onSelect({
-      selectedTab: tabValue,
+      selectedTab: activeTab.value,
       begin: beginDate,
       end: endDate,
+      serviceTimeId: activeTab.selectedServiceTime
+        ? activeTab.selectedServiceTime.value
+        : null,
     })
+  }
+
+  const handleSelectServiceTime = () => {
+    SelectPicker.render({
+      data: serviceTimeList,
+      value: activeTab.selectedServiceTime.value,
+    }).then((value) => {
+      const serviceTime = _.find(
+        serviceTimeList,
+        (item) => item.value === value
+      )
+      const { min, max } = getServiceTimeRange(serviceTime)
+      if (
+        moment(beginDate) < moment(min) ||
+        moment(beginDate) > moment(max) ||
+        moment(endDate) < moment(min) ||
+        moment(endDate) > moment(max)
+      ) {
+        setBeginDate(max)
+        setEndDate(max)
+      }
+      setActiveTab({
+        ...activeTab,
+        min,
+        max,
+        selectedServiceTime: serviceTime,
+      })
+    })
+  }
+
+  const handleTabChange = (value) => {
+    if (value !== activeTab.value) {
+      const tab = _.find(tabs, (tab) => tab.value === value)
+      setActiveTab(tab)
+      setBeginDate(tab.max)
+      setEndDate(tab.max)
+    }
   }
 
   return (
     <div className='m-tab-date-select'>
-      <Tabs
-        tabs={tabs}
-        active={tabValue}
-        onChange={(value) => setTabValue(value)}
-      />
+      <Tabs tabs={tabs} active={activeTab.value} onChange={handleTabChange} />
+      {activeTab.selectedServiceTime && (
+        <Flex alignCenter justifyCenter>
+          <Flex
+            alignCenter
+            justifyCenter
+            className='m-text-desc m-padding-tb-5 m-padding-lr-10 m-bg-back m-border-radius'
+            onClick={handleSelectServiceTime}
+          >
+            <div className='m-margin-right-5'>{`${getLocale('运营时间')}:${
+              activeTab.selectedServiceTime.text
+            }`}</div>
+            <SvgArrowDown />
+          </Flex>
+        </Flex>
+      )}
       <div className='m-tab-date-select-calendar'>
         <RangeCalendar
           ref={calendarRef}
           begin={beginDate}
           end={endDate}
-          min={_selectedTab.min}
-          max={_selectedTab.max}
+          min={activeTab.min}
+          max={activeTab.max}
           showDateLabel
           onSelect={({ begin, end }) => {
             setBeginDate(begin)
@@ -50,7 +130,7 @@ const TabDateSelect = ({ tabs, selectedTab, begin, end, onSelect }) => {
       </div>
 
       <Flex alignCenter justifyCenter className='m-padding-tb-10'>
-        <div>{_selectedTab.text}</div>
+        <div>{activeTab.text}</div>
         <div className='m-text-bold m-bg-back m-margin-left-10 m-padding-tb-5 m-padding-lr-10 m-border-radius'>{`${moment(
           beginDate
         ).format('YYYY-MM-DD')} ~ ${moment(endDate).format(
@@ -108,6 +188,8 @@ TabDateSelect.propTypes = {
   end: PropTypes.object,
   /** 确定回调 */
   onSelect: PropTypes.func.isRequired,
+  /** 运营周期列表 */
+  serviceTimeList: PropTypes.array,
 }
 
 export default TabDateSelect
