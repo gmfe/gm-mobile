0762b220059cf30b32751915c938bba48ce32452
liyatang <liyatang@qq.com>
2020-06-16 19:09:57 +0800

    fix flex

--- packages/components/src/component/flex/index.js
+++ packages/components/src/component/flex/index.js
@@ -3,94 +3,99 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import View from '../view'
 
-const Flex = ({
-  flex,
-
-  auto,
-  none,
-  width,
-  height,
-
-  row,
-  column,
-
-  wrap,
-  nowrap,
-
-  justifyStart,
-  justifyEnd,
-  justifyCenter,
-  justifyBetween,
-  justifyAround,
-
-  alignStart,
-  alignEnd,
-  alignCenter,
-  alignBaseline,
-  alignStretch,
-
-  alignContentStart,
-  alignContentEnd,
-  alignContentCenter,
-  alignContentBetween,
-  alignContentAround,
-  alignContentStretch,
-
-  className,
-  style,
-
-  ...rest
-}) => {
-  const cn = classNames(
+const Flex = React.forwardRef(
+  (
     {
-      'm-flex': true,
+      flex,
 
-      'm-flex-flex': flex,
-      'm-flex-auto': auto,
-      'm-flex-none': none || width || height,
+      auto,
+      none,
+      width,
+      height,
 
-      'm-flex-row': row,
-      'm-flex-column': column,
+      row,
+      column,
 
-      'm-flex-wrap': wrap,
-      'm-flex-nowrap': nowrap,
+      wrap,
+      nowrap,
 
-      'm-flex-justify-start': justifyStart,
-      'm-flex-justify-end': justifyEnd,
-      'm-flex-justify-center': justifyCenter,
-      'm-flex-justify-between': justifyBetween,
-      'm-flex-justify-around': justifyAround,
+      justifyStart,
+      justifyEnd,
+      justifyCenter,
+      justifyBetween,
+      justifyAround,
 
-      'm-flex-align-start': alignStart,
-      'm-flex-align-end': alignEnd,
-      'm-flex-align-center': alignCenter,
-      'm-flex-align-baseline': alignBaseline,
-      'm-flex-align-stretch': alignStretch,
+      alignStart,
+      alignEnd,
+      alignCenter,
+      alignBaseline,
+      alignStretch,
 
-      'm-flex-align-content-start': alignContentStart,
-      'm-flex-align-content-end': alignContentEnd,
-      'm-flex-align-content-center': alignContentCenter,
-      'm-flex-align-content-between': alignContentBetween,
-      'm-flex-align-content-around': alignContentAround,
-      'm-flex-align-content-stretch': alignContentStretch,
+      alignContentStart,
+      alignContentEnd,
+      alignContentCenter,
+      alignContentBetween,
+      alignContentAround,
+      alignContentStretch,
+
+      className,
+      style,
+
+      ...rest
     },
-    className
-  )
+    ref
+  ) => {
+    const cn = classNames(
+      {
+        'm-flex': true,
 
-  const s = Object.assign({}, style)
-  if (flex) {
-    s.flex = typeof flex === 'boolean' ? 1 : flex
-    s.WebKitFlex = typeof flex === 'boolean' ? 1 : flex
-  }
-  if (height) {
-    s.height = height
-  }
-  if (width) {
-    s.width = width
-  }
+        'm-flex-flex': flex,
+        'm-flex-auto': auto,
+        'm-flex-none': none || width || height,
 
-  return <View {...rest} className={cn} style={s} />
-}
+        'm-flex-row': row,
+        'm-flex-column': column,
+
+        'm-flex-wrap': wrap,
+        'm-flex-nowrap': nowrap,
+
+        'm-flex-justify-start': justifyStart,
+        'm-flex-justify-end': justifyEnd,
+        'm-flex-justify-center': justifyCenter,
+        'm-flex-justify-between': justifyBetween,
+        'm-flex-justify-around': justifyAround,
+
+        'm-flex-align-start': alignStart,
+        'm-flex-align-end': alignEnd,
+        'm-flex-align-center': alignCenter,
+        'm-flex-align-baseline': alignBaseline,
+        'm-flex-align-stretch': alignStretch,
+
+        'm-flex-align-content-start': alignContentStart,
+        'm-flex-align-content-end': alignContentEnd,
+        'm-flex-align-content-center': alignContentCenter,
+        'm-flex-align-content-between': alignContentBetween,
+        'm-flex-align-content-around': alignContentAround,
+        'm-flex-align-content-stretch': alignContentStretch,
+      },
+      className
+    )
+
+    const s = Object.assign({}, style)
+    if (flex) {
+      s.flex = typeof flex === 'boolean' ? 1 : flex
+      s.WebKitFlex = typeof flex === 'boolean' ? 1 : flex
+    }
+    if (height) {
+      s.height = height
+    }
+    if (width) {
+      s.width = width
+    }
+
+    return <View ref={ref} {...rest} className={cn} style={s} />
+  }
+)
 
 Flex.propTypes = {
   flex: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
