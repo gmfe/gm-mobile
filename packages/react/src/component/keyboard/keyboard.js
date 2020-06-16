3fbe2a62fe9efd1efacbaeae425ed1ac113de799
xumeimei <xumeimei@guanmai.cn>
2020-06-16 16:31:55 +0800

    keyboard

--- packages/react/src/component/keyboard/keyboard.js
+++ packages/react/src/component/keyboard/keyboard.js
@@ -44,6 +44,8 @@ const Keyboard = (props) => {
     max,
     precision,
     getErrorMsg,
+    disabledHeader,
+    onChange,
     ...rest
   } = props
 
@@ -151,25 +153,29 @@ const Keyboard = (props) => {
     const cv = checkValue(v)
 
     setCurrentValue(cv)
-    // onSubmit(cv)
+    onChange(cv)
   }
 
   return (
     <Flex column {...rest} className='m-number-keyboard'>
-      <Flex className='m-number-keyboard-header'>
-        <Flex alignCenter className='m-number-keyboard-header-input m-text-18'>
-          {currentValue}
+      {!disabledHeader && (
+        <Flex className='m-number-keyboard-header'>
+          <Flex
+            alignCenter
+            className='m-number-keyboard-header-input m-text-18'
+          >
+            {currentValue}
+          </Flex>
+          <Button
+            type='primary'
+            mini
+            className='m-number-keyboard-header-btn'
+            onClick={handleSubmit}
+          >
+            {getLocale('确定')}
+          </Button>
         </Flex>
-        <Button
-          type='primary'
-          mini
-          className='m-number-keyboard-header-btn'
-          onClick={handleSubmit}
-        >
-          {getLocale('确定')}
-        </Button>
-      </Flex>
-      {children}
+      )}
       <BaseKeyboard onChange={handleValueChange} />
     </Flex>
   )
@@ -179,7 +185,7 @@ Keyboard.propTypes = {
   /** 初始默认值 */
   defaultValue: PropTypes.string,
   /** 确定回调函数 */
-  onSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func,
   /** 最小值 */
   min: PropTypes.number,
   /** 最大值 */
@@ -191,12 +197,18 @@ Keyboard.propTypes = {
    * 否则返回null
    */
   getErrorMsg: PropTypes.func,
+
+  disabledHeader: PropTypes.bool,
+  /** 键盘点击回调，参数为点击输入值 */
+  onChange: PropTypes.func,
 }
 
 Keyboard.defaultProps = {
   defaultValue: '',
   precision: 2,
   getErrorMsg: handleErrorMsg,
+  onChange: _.noop,
+  onSubmit: _.noop,
 }
 
 export default Keyboard
