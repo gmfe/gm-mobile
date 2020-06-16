e0875ff3f3fad543de4fa3d06574a2e67ee91a09
liyatang <liyatang@qq.com>
2020-06-16 12:09:44 +0800

    cell

--- mini-program/src/pages/index/index.js
+++ mini-program/src/pages/index/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Navigator } from '@tarojs/components'
+import { Text } from '@tarojs/components'
 import map from 'lodash/map'
 import each from 'lodash/each'
 import './index.less'
