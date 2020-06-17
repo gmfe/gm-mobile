33d06d8f95d06f9f0b6c9a94621999c1b233211a
hejunze <hejunze@guanmai.cn>
2020-06-17 16:49:23 +0800

    fix

--- packages/mp/src/component/counter/index.js
+++ packages/mp/src/component/counter/index.js
@@ -3,7 +3,8 @@ import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { Input } from '@tarojs/components'
 import { View, Toast } from '@gm-mobile/components'
-import _ from 'lodash'
+import isNaN from 'lodash/isNaN'
+import includes from 'lodash/includes'
 import Big from 'big.js'
 
 const Counter = ({
@@ -23,7 +24,7 @@ const Counter = ({
     if (value === '') {
       return 0
     }
-    return _.isNaN(parseFloat(value)) ? '' : parseFloat(value)
+    return isNaN(parseFloat(value)) ? '' : parseFloat(value)
   }
 
   const plusDisabled = max && text2Number(value) >= max
@@ -47,7 +48,7 @@ const Counter = ({
 
   const handleChange = (type) => {
     let v = text2Number(value)
-    const _precision = _.includes(value, '.') ? precision : 0
+    const _precision = includes(value, '.') ? precision : 0
     if (type === 'minus') {
       if (minusDisabled) return
       // 小于0时展示为0不变
