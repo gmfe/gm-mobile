a6bab721c0f7bae90257c6872f77a55842305212
liyatang <liyatang@qq.com>
2020-06-17 16:12:07 +0800

    layoutroot

--- mini-program/src/pages/index/index.js
+++ mini-program/src/pages/index/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { Text } from '@tarojs/components'
 import map from 'lodash/map'
 import each from 'lodash/each'
 import './index.less'
 import queryString from 'query-string'
 import {
+  View,
+  Text,
   Page,
   Cells,
   Cell,
@@ -71,38 +72,41 @@ each(storiesList, ({ module, packageName, path }) => {
 export default class Index extends Component {
   render() {
     return (
-      <Page>
-        {map(dataMap, (oneValue, oneKey) => {
-          return (
-            <Cells key={oneKey} title={oneKey}>
-              {map(oneValue, (twoValue, twoKey) => {
-                return (
-                  <Cell
-                    access
-                    key={twoKey}
-                    onClick={() => {
-                      ActionSheet.render({
-                        title: twoKey,
-                        data: map(twoValue, (thereValue, thereKey) => ({
-                          text: thereKey,
-                          value: queryString.stringify(thereValue),
-                        })),
-                      }).then((value) => {
-                        wx.navigateTo({
-                          url: `/pages/index/stories?${value}`,
+      <View>
+        <Page>
+          {map(dataMap, (oneValue, oneKey) => {
+            return (
+              <Cells key={oneKey} title={oneKey}>
+                {map(oneValue, (twoValue, twoKey) => {
+                  return (
+                    <Cell
+                      access
+                      key={twoKey}
+                      onClick={() => {
+                        ActionSheet.render({
+                          title: twoKey,
+                          data: map(twoValue, (thereValue, thereKey) => ({
+                            text: thereKey,
+                            value: queryString.stringify(thereValue),
+                          })),
+                        }).then((value) => {
+                          wx.navigateTo({
+                            url: `/pages/index/stories?${value}`,
+                          })
                         })
-                      })
-                    }}
-                  >
-                    <Text>{twoKey}</Text>
-                  </Cell>
-                )
-              })}
-            </Cells>
-          )
-        })}
+                      }}
+                    >
+                      <Text>{twoKey}</Text>
+                    </Cell>
+                  )
+                })}
+              </Cells>
+            )
+          })}
+          <View>asfasfasfas</View>
+        </Page>
         <LayoutRoot />
-      </Page>
+      </View>
     )
   }
 }
