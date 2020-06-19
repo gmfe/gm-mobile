ca13b2735a7ff06ae8d290a6cb56d09983285b19
electhuang <electhuang@gmail.com>
2020-06-19 10:43:10 +0800

    progress_bar

--- packages/react/src/component/progress_bar/index.js
+++ packages/react/src/component/progress_bar/index.js
@@ -8,23 +8,29 @@ const ProgressBar = ({
   percentage,
   text,
   textInside,
+  strokeWidth,
   ...rest
 }) => {
+  const percent = percentage > 100 ? 100 : percentage
+
   return (
     <div {...rest} className={classNames('m-progress', className)}>
       <div className='m-progress-bar'>
-        <div className='m-progress-bar-outer'>
+        <div
+          className='m-progress-bar-outer'
+          style={{ height: `${strokeWidth}px` }}
+        >
           <div
             className='m-progress-bar-inner'
-            style={{ width: `${percentage}%` }}
+            style={{ width: `${percent}%` }}
           >
-            {showText && textInside && <div>{text || `${percentage}%`}</div>}
+            {showText && textInside && <div>{text || `${percent}%`}</div>}
           </div>
         </div>
       </div>
 
       {showText && !textInside && (
-        <div className='m-progress-bar-text'>{text || `${percentage}%`}</div>
+        <div className='m-progress-bar-text'>{text || `${percent}%`}</div>
       )}
     </div>
   )
@@ -36,6 +42,8 @@ ProgressBar.propTypes = {
   /** 是否显示文字 */
   showText: PropTypes.bool,
   className: PropTypes.string,
+  /** 进度条高度 */
+  strokeWidth: PropTypes.number,
   /** 百分比 */
   percentage: PropTypes.number.isRequired,
   /** 显示的文字 */
@@ -44,6 +52,7 @@ ProgressBar.propTypes = {
 
 ProgressBar.defaultProps = {
   textInside: false,
+  strokeWidth: 6,
 }
 
 export default ProgressBar
