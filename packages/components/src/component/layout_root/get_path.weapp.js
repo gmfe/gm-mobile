59940bf1f6ca266db527bb01227f57ff0e32c918
hejunze <hejunze@guanmai.cn>
2020-06-23 14:40:28 +0800

    fix

--- packages/components/src/component/layout_root/get_path.weapp.js
+++ packages/components/src/component/layout_root/get_path.weapp.js
@@ -2,6 +2,6 @@ import { getCurrentPages } from '@tarojs/taro'
 import _last from 'lodash/last'
 
 export default function getPath() {
-  const route = _last(getCurrentPages()).__route__
+  const route = _last(getCurrentPages()).route
   return `/${route}`
 }
