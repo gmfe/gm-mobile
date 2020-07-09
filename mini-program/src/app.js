6ba84cf58fda10b41448ce8ce77ba491623848e4
liyatang <liyatang@qq.com>
2020-07-09 15:08:20 +0800

    fix lodash

--- mini-program/src/app.js
+++ mini-program/src/app.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 import './app.less'
 
+Object.assign(global, {
+  Array: Array,
+  Date: Date,
+  Error: Error,
+  Function: Function,
+  Math: Math,
+  Object: Object,
+  RegExp: RegExp,
+  String: String,
+  TypeError: TypeError,
+  setTimeout: setTimeout,
+  clearTimeout: clearTimeout,
+  setInterval: setInterval,
+  clearInterval: clearInterval,
+})
+
 class App extends Component {
   componentDidMount() {}
 
