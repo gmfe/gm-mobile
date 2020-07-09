a4534238ec9670fc36a001bd53305d17e53cb99d
liyatang <liyatang@qq.com>
2020-07-09 18:05:39 +0800

    variable

--- packages/mp/src/component/safe_header/index.js
+++ packages/mp/src/component/safe_header/index.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 import { View } from '@tarojs/components'
+import classNames from 'classnames'
+import UtilMP from '../../util'
 
-const SafeHeaderMP = ({ style, children, ...rest }) => {
+const SafeHeaderMP = ({ className, style, children, ...rest }) => {
   const [right, setRight] = useState(100)
 
   useEffect(() => {
     // eslint-disable-next-line
-    const rect = wx.getMenuButtonBoundingClientRect()
+    const rect = UtilMP.getMenuButtonBoundingClientRect()
 
     // eslint-disable-next-line
-    wx.getSystemInfo({
-      success: (res) => {
-        setRight(res.windowWidth - rect.left)
-      },
+    UtilMP.getSystemInfo().then((info) => {
+      setRight(info.windowWidth - rect.left)
     })
   }, [])
 
   return (
-    <View {...rest} style={{ ...style, paddingRight: right }}>
+    <View
+      {...rest}
+      style={{ ...style, paddingRight: right }}
+      className={classNames('m-safe-header', className)}
+    >
       {children}
     </View>
   )
+}
+
+SafeHeaderMP.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
 }
 
 export default SafeHeaderMP
