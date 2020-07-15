46f7ff83d22d1e82640ee8db9bf2d0b61e9b0ae4
liyatang <liyatang@qq.com>
2020-07-15 12:15:17 +0800

    status

---
+++ packages/components/src/component/status/stories.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Status from './'
+import View from '../view'
+
+export const Normal = () => {
+  return (
+    <View>
+      <Status type='loading' />
+      <Status
+        type='error'
+        onReload={() => {
+          console.log('reload')
+        }}
+      />
+      <Status type='empty' />
+    </View>
+  )
+}
+
+export default {
+  title: '布局/Status',
+}
