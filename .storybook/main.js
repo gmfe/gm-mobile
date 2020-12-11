d3f6a7a43868cb2c9e475e928d3df16d187fea40
hejunze <hejunze@guanmai.cn>
2020-12-11 17:40:45 +0800

    update: update tsconfig about storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,40 @@
+const path = require('path')
+const _ = require('lodash')
+
 const webpackFinal = (config) => {
+  _.each(config.module.rules, (rule) => {
+    if (rule.use && rule.use[0] && rule.use[0].loader) {
+      if (rule.use[0].loader.includes('babel-loader')) {
+        rule.include.push(/gm-/)
+        rule.exclude = function (filepath) {
+          return filepath.includes('/node_modules/')
+        }
+      }
+    }
+
+    if (rule.loader && rule.loader.includes('file-loader')) {
+      rule.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+    }
+  })
+
   config.module.rules.push({
     test: /\.stories\.jsx?$/,
     loaders: [require.resolve('@storybook/source-loader')],
     enforce: 'pre',
   })
 
-  config.module.rules[3] = {
-    test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf|svg)(\?.*)?$/,
-    loader:
-      './node_modules/@storybook/core/node_modules/file-loader/dist/cjs.js',
-    query: { name: 'static/media/[name].[hash:8].[ext]' },
-  }
+  config.module.rules.push({
+    test: /(gm-mobile-icons)\.(woff|woff2|ttf|eot|svg)($|\?)/,
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          limit: 1024,
+          name: 'static/media/font/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  })
 
   config.module.rules.push({
     test: /\.less$/,
@@ -25,12 +49,6 @@ const webpackFinal = (config) => {
         loader: 'less-loader',
       },
     ],
-  })
-
-  config.module.rules.push({
-    test: /\.stories\.jsx?$/,
-    loaders: [require.resolve('@storybook/source-loader')],
-    enforce: 'pre',
   })
 
   config.module.rules.unshift({
@@ -61,8 +79,36 @@ const webpackFinal = (config) => {
 }
 
 module.exports = {
+  addons: [
+    '@storybook/addon-storysource',
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          transpileOnly: true,
+          happyPackMode: true,
+          configFile: path.resolve(__dirname, '../tsconfig.json'),
+        },
+        forkTsCheckerWebpackPluginOptions: {
+          checkSyntacticErrors: true,
+          tsconfig: path.resolve(__dirname, '../tsconfig.json'),
+          reportFiles: [
+            'packages/c-react/src/**/*stories.{tsx,ts}',
+            'packages/c-business/src/**/*stories.{tsx,ts}',
+            'packages/react/src/**/*stories.{tsx,ts}',
+            'packages/locales/src/**/*stories.{tsx,ts}',
+            'packages/business/src/**/*stories.{tsx,ts}',
+            'packages/c-service-time/src/**/*stories.{tsx,ts}',
+            'packages/swiper/src/**/*stories.{tsx,ts}',
+            'packages/c-qrcode/src/**/*stories.{tsx,ts}',
+            'other/**/*.stories.{tsx,ts}',
+          ],
+        },
+      },
+    },
+  ],
   // 枚举，避免识别到 node_modules 的 stories
-  // todo: 暂时兼容js和tsx
+  // todo: 暂时兼容js和ts
   stories: [
     '../packages/c-react/src/**/*stories.js',
     '../packages/c-business/src/**/*stories.js',
@@ -74,15 +120,15 @@ module.exports = {
     '../packages/c-qrcode/src/**/*stories.js',
     '../other/**/*.stories.js',
 
-    '../packages/c-react/src/**/*stories.tsx',
-    '../packages/c-business/src/**/*stories.tsx',
-    '../packages/react/src/**/*stories.tsx',
-    '../packages/locales/src/**/*stories.tsx',
-    '../packages/business/src/**/*stories.tsx',
-    '../packages/c-service-time/src/**/*stories.tsx',
-    '../packages/swiper/src/**/*stories.tsx',
-    '../packages/c-qrcode/src/**/*stories.tsx',
-    '../other/**/*.stories.tsx',
+    '../packages/c-react/src/**/*stories.{tsx,ts}',
+    '../packages/c-business/src/**/*stories.{tsx,ts}',
+    '../packages/react/src/**/*stories.{tsx,ts}',
+    '../packages/locales/src/**/*stories.{tsx,ts}',
+    '../packages/business/src/**/*stories.{tsx,ts}',
+    '../packages/c-service-time/src/**/*stories.{tsx,ts}',
+    '../packages/swiper/src/**/*stories.{tsx,ts}',
+    '../packages/c-qrcode/src/**/*stories.{tsx,ts}',
+    '../other/**/*.stories.{tsx,ts}',
   ],
   webpackFinal,
 }
