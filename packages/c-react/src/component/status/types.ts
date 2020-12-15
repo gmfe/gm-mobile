07bc2c1355681e5324e2cd24b79f520568d63f16
michwh <fjsxhwh@163.com>
2020-12-15 19:32:56 +0800

    fix: 修改注释

--- packages/c-react/src/component/status/types.ts
+++ packages/c-react/src/component/status/types.ts
@@ -3,7 +3,8 @@ import { HTMLAttributes } from 'react'
 interface StatusProps extends HTMLAttributes<HTMLDivElement> {
   type: 'loading' | 'error' | 'empty'
   tip?: string
-  onReload?: () => void // type === error 有效
+  /** type === error 有效 */
+  onReload?: () => void
 }
 
 export type { StatusProps }
