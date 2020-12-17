3c79f6ecfd7e1640f39561c7ddaa27a98a6e51e5
shengqiwu <shengqiwu@guanmai.cn>
2020-12-17 19:55:21 +0800

    fix: loading

--- packages/c-react/src/component/loading/index.weapp.tsx
+++ packages/c-react/src/component/loading/index.weapp.tsx
@@ -6,7 +6,7 @@ import SVGLoading2 from './loading2.svg'
 import { Image } from '@tarojs/components'
 
 interface LoadingProps extends ViewProps {
-  // 专门给 Toast 用，Loading 是 图片，不能变颜色，toast 是灰度，估需要用白色loading
+  /** 专门给 Toast 用，Loading 是 图片，不能变颜色，toast 是灰度，估需要用白色loading */
   _isToast?: boolean
 }
 
