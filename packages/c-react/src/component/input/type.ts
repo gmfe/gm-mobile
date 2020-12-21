4de2ab320559a29072a9409fe1906eab71ab9242
shengqiwu <shengqiwu@guanmai.cn>
2020-12-21 14:48:56 +0800

    fix: input

--- packages/c-react/src/component/input/type.ts
+++ packages/c-react/src/component/input/type.ts
@@ -11,16 +11,19 @@ type InputPasswordProps = Omit<InputProps, 'onInput'>
 type InputProps = {
   value: string
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
-  onInput?: (e: BaseEventOrig<TaroInputProps.inputEventDetail>) => void
   isForm?: boolean
   className?: string
   focus?: boolean
-  type?: 'text' | 'password' | 'number'
+  type?: 'text' | 'password' | 'number' | 'idcard' | 'digit'
   placeholderClass?: string
   placeholder?: string
   disabled?: boolean
   style?: CSSProperties
   password?: boolean
+
+  /* 小程序特有 */
+  onInput?: (e: BaseEventOrig<TaroInputProps.inputEventDetail>) => void
+  confirmType?: 'send' | 'search' | 'next' | 'go' | 'done'
 }
 
 export type { InputProps, InputMaxLengthProps, InputPasswordProps }
