46351ebdfe8b0664c7bc7bff5ac8c20587338dbb
xumeimei <xumeimei@guanmai.cn>
2020-12-28 20:05:04 +0800

    fix: some

--- global.d.ts
+++ global.d.ts
@@ -9,7 +9,12 @@ declare module '*.gif'
 declare module '*.bmp'
 
 /** cookie */
-declare module 'weapp-cookie'
+declare module 'weapp-cookie' {
+  export function initDomain(domain: string): void
+  export function get(key: string, domain: string): any
+  export function set(key: string, value: string, options: any): void
+  export function remove(key: string, domain: string): void
+}
 
 /** 其它需要后续添加 */
 interface Window {
