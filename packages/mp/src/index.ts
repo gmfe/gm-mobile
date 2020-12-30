7d5117149e79129ce2a669c40d6ac202122a0d73
xumeimei <xumeimei@guanmai.cn>
2020-12-30 16:10:33 +0800

    WIP: export

--- packages/mp/src/index.ts
+++ packages/mp/src/index.ts
@@ -1,114 +1,8 @@
 import LocalStorage from './storage'
-import SafeHeaderMP from './component/safe_header'
-
 import useFirstDidShow from './hook/use_first_did_show'
 
-import {
-  // 不提供，请用 PageMP
-  // LayoutRoot,
-  // Page,
-  Button,
-  ButtonTime,
-  Flex,
-  Loading,
-  Mask,
-  Text,
-  Toast,
-  View,
-  Label,
-  Tabs,
-  Badge,
-  Switch,
-  TagWrap,
-  Square,
-  RangeCalendar,
-  MultipleCalendar,
-  Calendar,
-  Dialog,
-  Checkbox,
-  Radio,
-  Divider,
-  Popup,
-  Tooltip,
-  ActionSheet,
-  Search,
-  FakeSearch,
-  SearchPage,
-  InnerLayer,
-  Input,
-  InputMaxLength,
-  InputPassword,
-  Textarea,
-  Cells,
-  Cell,
-  CellForm,
-  CellsForm,
-  Picker,
-  CouplingPicker,
-  ConfirmPicker,
-  ConfirmCouplingPicker,
-  SelectPicker,
-  RepeatTimes,
-  Price,
-  Nav,
-  VList,
-  Status,
-} from '@gm-mobile/c-react'
-
-export {
-  // 不提供，请用 PageMP
-  // LayoutRoot,
-  // Page,
-  Button,
-  ButtonTime,
-  Flex,
-  Loading,
-  Mask,
-  Text,
-  Toast,
-  View,
-  Label,
-  Tabs,
-  Badge,
-  Switch,
-  TagWrap,
-  Square,
-  RangeCalendar,
-  MultipleCalendar,
-  Calendar,
-  Dialog,
-  Checkbox,
-  Radio,
-  Divider,
-  Popup,
-  Tooltip,
-  ActionSheet,
-  Search,
-  FakeSearch,
-  SearchPage,
-  InnerLayer,
-  Input,
-  InputMaxLength,
-  InputPassword,
-  Textarea,
-  Cells,
-  Cell,
-  CellForm,
-  CellsForm,
-  Picker,
-  CouplingPicker,
-  ConfirmPicker,
-  ConfirmCouplingPicker,
-  SelectPicker,
-  RepeatTimes,
-  LocalStorage,
-  Price,
-  Nav,
-  VList,
-  Status,
-  SafeHeaderMP,
-  useFirstDidShow,
-}
+/** 不提供LayoutRoot, Page，请用 PageMP */
+export * from '@gm-mobile/c-react'
 
 export * from './component/image'
 export * from './component/page'
@@ -116,3 +10,5 @@ export * from './component/counter'
 export * from './component/status_bar'
 export * from './component/map'
 export * from './component/swiper'
+export * from './component/safe_header'
+export { LocalStorage, useFirstDidShow }
