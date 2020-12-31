8e3f46440941ca58a157b1025b5f44c29dcebacc
michwh <fjsxhwh@163.com>
2020-12-31 11:47:51 +0800

    fix: status_bar导出props

--- packages/mp/src/component/status_bar/status_bar.tsx
+++ packages/mp/src/component/status_bar/status_bar.tsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState, memo, FC, HtmlHTMLAttributes } from 'react'
+import React, { useEffect, useState, memo, FC, HTMLAttributes } from 'react'
 import { View } from '@gm-mobile/c-react'
 import UtilMP from '../../util'
 
-const StatusBarMP: FC<HtmlHTMLAttributes<HTMLDivElement>> = memo(
-  (style, ...rest) => {
-    const [height, setHeight] = useState(20)
-    useEffect(() => {
-      const rect = UtilMP.getMenuButtonBoundingClientRect()
+export type StatusBarMPProps = HTMLAttributes<HTMLDivElement>
 
-      UtilMP.getSystemInfo().then((res) => {
-        // 不一定能读到胶囊,或者胶囊占据整个宽， 此时不需要设置statusBar
-        return setHeight(rect?.left ? res.statusBarHeight : 0)
-      })
-    }, [])
-    if (!height) return null
-    return (
-      <View
-        {...rest}
-        style={{
-          height,
-          ...style,
-        }}
-      />
-    )
-  }
-)
+const StatusBarMP: FC<StatusBarMPProps> = memo((style, ...rest) => {
+  const [height, setHeight] = useState(20)
+  useEffect(() => {
+    const rect = UtilMP.getMenuButtonBoundingClientRect()
+
+    UtilMP.getSystemInfo().then((res) => {
+      // 不一定能读到胶囊,或者胶囊占据整个宽， 此时不需要设置statusBar
+      return setHeight(rect?.left ? res.statusBarHeight : 0)
+    })
+  }, [])
+  if (!height) return null
+  return (
+    <View
+      {...rest}
+      style={{
+        height,
+        ...style,
+      }}
+    />
+  )
+})
 
 export default StatusBarMP
