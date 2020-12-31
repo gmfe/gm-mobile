532cf6a97d8a2010bde3791f909e3bcc2d0b97a1
liyatang <liyatang@qq.com>
2020-12-31 18:29:03 +0800

    WIP: request

--- packages/mp-request/src/index.ts
+++ packages/mp-request/src/index.ts
@@ -1,6 +1,6 @@
 import { instance, Request } from './request'
 
-import configHeaders from './config_header'
+import configHeaders from './config_headers'
 import configTrace from './config_trace'
 import configError from './config_error'
 import configProgress from './config_progress'
