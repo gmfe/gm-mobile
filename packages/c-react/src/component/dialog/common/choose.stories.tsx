1e235dc627ec3658732c37303312c42b7aeaf848
jawei <1005945485@qq.com>
2021-07-08 15:57:29 +0800

    update the story

--- packages/c-react/src/component/dialog/common/choose.stories.tsx
+++ packages/c-react/src/component/dialog/common/choose.stories.tsx
@@ -5,7 +5,7 @@ import choose, { ChooseProps } from './choose'
 
 export const Choose = (args: ChooseProps) => {
   return (
-    <View>
+    <View style={{ minHeight: '600px' }}>
       <Button
         className='m-margin-10'
         onClick={() => {
@@ -20,28 +20,38 @@ export const Choose = (args: ChooseProps) => {
     </View>
   )
 }
-Choose.args = {
-  list: Array(10)
-    .fill(null)
-    .map((_, i) => {
-      return {
-        text: 'LABEL-' + i,
-        value: i,
-      }
-    }),
-  multiSelect: false,
-  needConfirm: false,
-  //   search: false,
-  title: 'Title',
-  defaultSelected: [{ text: '', value: 0 }],
-} as ChooseProps
 
 export default {
   title: '浮层/Dialog/Choose',
+  component: choose,
+  args: {
+    list: Array(10)
+      .fill(null)
+      .map((_, i) => {
+        return {
+          text: 'LABEL-' + i,
+          value: i,
+        }
+      }),
+    multiSelect: false,
+    needConfirm: false,
+    //   search: false,
+    title: 'Title',
+    defaultSelected: [{ text: '', value: 0 }],
+  } as ChooseProps,
+  argTypes: {
+    multiSelect: {
+      description: '是否显示确认按钮，多选时multiSelect强制为true',
+    },
+  },
   parameters: {
     docs: {
       source: {
-        type: 'auto',
+        type: 'code',
+        code: `
+choose(options).then((res) => {
+  return alert(JSON.stringify(res))
+})`,
       },
     },
   },
