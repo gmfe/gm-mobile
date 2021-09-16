cacf59ada02236459385778ad74bcb00bc38e357
jawei <1005945485@qq.com>
2021-09-16 15:33:27 +0800

    修复引用问题

--- packages/mp/src/index.ts
+++ packages/mp/src/index.ts
@@ -1,5 +1,7 @@
 import LocalStorage from './storage'
 import useFirstDidShow from './hook/use_first_did_show'
+import Router from './router'
+import upload from './uploader'
 
 /** 不提供LayoutRoot, Page，请用 PageMP */
 export * from '@gm-mobile/c-react'
@@ -10,6 +12,5 @@ export * from './component/counter'
 export * from './component/status_bar'
 export * from './component/swiper'
 export * from './component/safe_header'
-export * as Router from './router'
-export * from './uploader'
-export { LocalStorage, useFirstDidShow }
+
+export { LocalStorage, useFirstDidShow, Router, upload }
