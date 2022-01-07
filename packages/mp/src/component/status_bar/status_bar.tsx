cfd7e1bfb7b18d1f1fba315cbba6b8cbdc218805
jawei <1005945485@qq.com>
2022-01-07 15:32:25 +0800

    fix: 优化StatusBar

--- packages/mp/src/component/status_bar/status_bar.tsx
+++ packages/mp/src/component/status_bar/status_bar.tsx
@@ -5,10 +5,9 @@ import UtilMP from '../../util'
 export type StatusBarMPProps = HTMLAttributes<HTMLDivElement>
 
 const StatusBarMP: FC<StatusBarMPProps> = memo((style, ...rest) => {
-  const [height, setHeight] = useState(20)
+  const [height, setHeight] = useState(wx.getSystemInfoSync().statusBarHeight)
   useEffect(() => {
     const rect = UtilMP.getMenuButtonBoundingClientRect()
-
     UtilMP.getSystemInfo().then((res) => {
       // 不一定能读到胶囊,或者胶囊占据整个宽， 此时不需要设置statusBar
       return setHeight(rect?.left ? res.statusBarHeight : 0)
