4540c08fb59492d4a062ca71989f41526b101bac
jawei <1005945485@qq.com>
2022-11-08 14:24:42 +0800

    fix: export

--- packages/mp-request/src/index.ts
+++ packages/mp-request/src/index.ts
@@ -4,7 +4,10 @@ import configHeaders from './config_headers'
 import configTrace from './config_trace'
 import configError from './config_error'
 import configProgress from './config_progress'
-import configPrivateDomain from './config_private_domain'
+import {
+  configPrivateDomain,
+  clearPrivateDomain,
+} from './config_private_domain'
 import { initAuth, clearAuth, setAccessToken } from './init'
 
 export {
@@ -18,5 +21,6 @@ export {
   configProgress,
   setAccessToken,
   configPrivateDomain,
+  clearPrivateDomain,
 }
 export type { Response } from './types'
