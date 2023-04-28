2269348d288e63f6281956375c111828c535de8c
heweizhi <heweizhi@guanmai.cn>
2023-04-28 14:54:26 +0800

    fix: SafeBottom的paddingBottom控制区间在[0,34]

--- packages/mp/src/component/safe_bottom/safe_bottom.tsx
+++ packages/mp/src/component/safe_bottom/safe_bottom.tsx
@@ -2,26 +2,34 @@ import React, { memo, FC } from 'react'
 import { View } from '@tarojs/components'
 import classNames from 'classnames'
 import { ViewProps } from '@tarojs/components/types/View'
+import _ from 'lodash'
 
 type SafeBottomMPProps = ViewProps
 
-const SafeBottomMP: FC<SafeBottomMPProps> = memo(
-  ({ className, children, ...rest }) => {
+const SafeBottomMP: FC<SafeBottomMPProps> = ({
+  className,
+  children,
+  ...rest
+}) => {
+  const getPaddingBottom = () => {
     const { safeArea, screenHeight } = wx.getSystemInfoSync()
-    const paddingBottom = screenHeight - safeArea.bottom
-
-    return (
-      <View className={classNames(className)} {...rest}>
-        {React.cloneElement((children as React.ReactElement) || <View />, {
-          style: {
-            paddingBottom:
-              paddingBottom > 0 ? `${paddingBottom}px!important` : undefined,
-          },
-        })}
-      </View>
-    )
+    const paddingBottom = 0 || Number(screenHeight || 0) - safeArea.bottom
+    /** 控制区间，禁止大于34 */
+    return _.clamp(paddingBottom, 0, 34)
   }
-)
+  return (
+    <View className={classNames(className)} {...rest}>
+      {React.cloneElement((children as React.ReactElement) || <View />, {
+        style: {
+          paddingBottom:
+            getPaddingBottom() > 0
+              ? `${getPaddingBottom()}px!important`
+              : undefined,
+        },
+      })}
+    </View>
+  )
+}
 
 export default SafeBottomMP
 export type { SafeBottomMPProps }
